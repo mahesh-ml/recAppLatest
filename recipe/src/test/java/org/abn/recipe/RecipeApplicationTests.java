80080b4d4b3ebcd9d0f25a027daccc8adc6e0c50
Mahesh Kumar <mkumar@unifonic.com>
2022-11-04 17:13:58 +0100

    read me updated

--- recipe/src/test/java/org/abn/recipe/RecipeApplicationTests.java
+++ recipe/src/test/java/org/abn/recipe/RecipeApplicationTests.java
@@ -2,11 +2,9 @@ package org.abn.recipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
-@ActiveProfiles
 class RecipeApplicationTests {
 
 	@Test
--- recipe/target/generated-sources/annotations/org/abn/recipe/config/RecipeMapperImpl.java
+++ recipe/target/generated-sources/annotations/org/abn/recipe/config/RecipeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-04T16:35:41+0100",
+    date = "2022-11-04T16:43:14+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 @Component
